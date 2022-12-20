cdabef785de9e75750d2f72539fc4623515276dd
kartik4gupta <97810590+kartik4gupta@users.noreply.github.com>
2022-12-20 17:55:08 +0530

    Update kernel_module.c

--- Kernel_Module/kernel_module.c
+++ Kernel_Module/kernel_module.c
@@ -3,6 +3,7 @@
 #include <linux/init.h>
 #include <linux/string.h>
 #include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 
 
@@ -13,7 +14,7 @@ MODULE_PARM_DESC(process_name, "The name of the process ");
 
 static int __init process_info_init(void)
 {
-    if (input_process == NULL) {
+    if (process == NULL) {
         return -1;
     }
 
@@ -21,15 +22,17 @@ static int __init process_info_init(void)
 
     for_each_process(task) {
         if (0==strcmp(process_name,task->comm)) {
-            printk(KERN_INFO "PID of process %s: %d\n", input_process, task->pid);
-            printk(KERN_INFO "PGID of process %s: %d\n", input_process, task->tgid);
-            printk(KERN_INFO "Command path of process %s: %s\n", input_process, task->mm->exe_file->f_path.dentry->d_iname);
-            printk(KERN_INFO "UID of process %s: %d\n", input_process, task->cred->uid);
+            printk("************************************************************\n");
+            printk(KERN_INFO "Path of process : %s\n", task->comm);
+            printk(KERN_INFO "PID of process : %d\n", task_pid_nr(task));
+            printk(KERN_INFO "PGID of process : %d\n", (int)task_tgid_nr(task));
+            printk(KERN_INFO "UID of process : %d\n", (int) task->cred->uid.val);
+            printk("************************************************************\n");
             return 0;
         }
     }
 
-    printk(KERN_INFO "Error in finding process with name %s\n", input_process);
+    printk(KERN_INFO "Error in finding process with name %s\n", process_name);
     return -1;
 }
 
